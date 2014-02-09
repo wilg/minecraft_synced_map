57fbf8f64761e3beda1f73d280ab0e544c260761
Wil Gieseler <wilgieseler@me.com>
2014-02-08 18:48:13 -0800

    hot new overlays

--- config/overviewer_config.py
+++ config/overviewer_config.py
@@ -15,19 +15,29 @@ def chestFilter(poi):
     if poi['id'] == "Chest":
         return ("Chest", "Chest with %d items" % len(poi['Items']))
 
-
 def diamondFilter(poi):
-    if poi['id'] == "Diamond":
-        return ("Diamond")
+    if poi['id'] == "Diamond Ore":
+        return ("Chest", "Big Buggs")
 
 markers = [
     dict(name="Signs", filterFunction=signFilter),
     dict(name="Players", filterFunction=playerIcons, checked=True)
     ]
 
-renders["overworld_day"] = {
+# I flipped what's considered reversed sorry
+renders["overworld_reverse"] = {
     "world": "world",
     "title": "Overworld",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection" : "lower-left",
+    'markers': markers,
+    "optimizeimg" : 1
+}
+
+renders["overworld_day"] = {
+    "world": "world",
+    "title": "Overworld (reverse)",
     "rendermode": smooth_lighting,
     "dimension": "overworld",
     "northdirection" : "upper-right",
@@ -35,15 +45,6 @@ renders["overworld_day"] = {
     "optimizeimg" : 1
 }
 
-renders["overworld_reverse"] = {
-    "world": "world",
-    "title": "Overworld (reverse)",
-    "rendermode": smooth_lighting,
-    "dimension": "overworld",
-    "northdirection" : "lower-left",
-    'markers': markers,
-    "optimizeimg" : 1
-}
 
 renders["caves"] = {
     "world": "world",
@@ -62,6 +63,33 @@ renders["nether"] = {
     "dimension": "nether",
     "northdirection" : "upper-right",
     'markers': markers,
+    "optimizeimg" : 1
+}
+
+renders['biome_overlay'] = {
+    'world': 'world',
+    'rendermode': [ClearBase(), BiomeOverlay()],
+    "northdirection" : "lower-left",
+    'title': "Biomes",
+    'overlay': ['overworld_reverse'],
+    "optimizeimg" : 1
+}
+
+renders['slime_overlay'] = {
+    'world': 'world',
+    'rendermode': [ClearBase(), SlimeOverlay()],
+    "northdirection" : "lower-left",
+    'title': "Slime",
+    'overlay': ['overworld_reverse'],
+    "optimizeimg" : 1
+}
+
+renders['mineral_overlay'] = {
+    'world': 'world',
+    'rendermode': [ClearBase(), MineralOverlay()],
+    "northdirection" : "lower-left",
+    'title': "Minerals",
+    'overlay': ['overworld_reverse'],
     "optimizeimg" : 1
 }
 
